bdac421323f328eaaacf9d9d279679e516da6d7c
GitHub <arif.working@gmail.com>
2013-03-08 17:44:09 +0700

    v1.0 matrixQPi.py simple code that will print the pressed key and a README

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ matrixQPi (Matrix Keypad for Raspberry Pi): a wiringPi-based code for reading in
 Run reading by:
 `$ python matrixQPi` 
 
-It will print the symbol of the pressed key found from the lookup. In this case the lookup contains symbols from regular phone dial-in key (digits 0, 1,..., 9 plus * and #)
+It can only read one pressed key at a time.
+
+It will print the symbol of the pressed key found from the lookup. In this case the lookup contains symbols from regular phone dial-in key (digits 0, 1,..., 9 plus * and # signs)
+
+As already stated above this requires wiringPi (`https://github.com/WiringPi/WiringPi-Python`) library. It has already tested using Raspberry Pi with Python 2.7.3rc2.
---
+++ matrixQPi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# version 1.0
+# read 3x4 membrane keypad and print pressed key to stdout
+
+import wiringpi
+import time
+INPUT=0
+OUTPUT=1
+HIGH=1
+LOW=0
+
+# mapping of symbols from the keypad button 
+# keypad used below is like phone dial key (10 digits and "*", "#" signs)
+keyPad=[
+[1,2,3],
+[4,5,6],
+[7,8,9],
+["*",0,"#"]
+]
+
+# mapping of used GPIO pin in wiringPi numbering to rows and columns
+row=[7,0,2,3]
+col=[4,5,6]
+
+SETUP=wiringpi.wiringPiSetup()
+# for debugging we can print SETUP status
+# print SETUP
+
+# (1) set all columns as output low
+for j in range(len(col)):
+	wiringpi.pinMode(col[j],OUTPUT)
+	wiringpi.digitalWrite(col[j],LOW)
+
+# (2) set all rows as input
+for i in range(len(row)):
+	wiringpi.pinMode(row[i],INPUT)
+
+# (3) scan rows for pushed key/button
+rowHi=1
+while rowHi==1:
+	for i in range(len(row)):
+		tmpRead=wiringpi.digitalRead(row[i])
+		if tmpRead==0:
+			rowHi=0
+			rowVal=i
+
+# (4) after finding which key/button from the row scans, convert columns to input
+for j in range(len(col)):
+        wiringpi.pinMode(col[j],INPUT)
+
+# (5) switch the i-th row found from scan to output
+wiringpi.pinMode(row[rowVal],OUTPUT)
+wiringpi.digitalWrite(row[rowVal],HIGH)
+
+# (6) scan columns for still-pushed key/button
+colLo=0
+while colLo==0:
+        for j in range(len(col)):
+                tmpRead=wiringpi.digitalRead(col[j])
+                if tmpRead==1:
+                        colLo=1
+			colVal=j
+
+# (7) print the symbol of pressed key, note that it prints newline 
+print keyPad[rowVal][colVal]
+
+# (7) reinitialize all rows and columns as input before exiting
+for i in range(len(row)):
+        wiringpi.pinMode(row[i],INPUT) 
+for j in range(len(col)):
+        wiringpi.pinMode(col[j],INPUT)
+
