bc63b982f03cc3519b00ca6fd9f2932c5ab2c353
Arif Kusbandono <arif.working@gmail.com>
2013-08-07 22:00:41 +0700

    v2.0 alpha class design

--- matrixQPi.py
+++ matrixQPi.py
@@ -6,24 +6,27 @@ OUTPUT=1
 HIGH=1
 LOW=0
 
-#mapping of used GPIO for LED indicator
-led=1
-
-# mapping of used GPIO pin in wiringPi numbering to rows and columns
-row=[7,0,2,3]
-col=[4,5,6]
-
-class matrixQPi:
+class matrixQConst:
+	def __init__(self):
+		# mapping of used GPIO pin in wiringPi numbering to rows and columns
+		self.row=[7,0,2,3]
+		self.col=[4,5,6]
+		#mapping of used GPIO for LED indicator
+		self.led=1
+		self.defaultKeyPad=[
+			[1,2,3],
+			[4,5,6],
+			[7,8,9],
+			["*",0,"#"]
+		]
+	
+class matrixQPi(object):
 	"""Simple matrix key row-column GPIO reader"""
 	# mapping of symbols from the keypad button
-	# keypad used below is like phone dial key (10 digits and "*", "#" signs)
-	keyPad=[
-		[1,2,3],
-		[4,5,6],
-		[7,8,9],
-		["*",0,"#"]
-	]
-	def __init__(self):
+	# default optional keypad with phone dial keys (10 digits and "*", "#" signs)
+	default =  matrixQConst()
+
+	def __init__(self, numberOfRow=4, numberOfCol=3, keyPad=default.defaultKeyPad, row=default.row, col=default.col):
 		self.SETUP=wiringpi.wiringPiSetup()
 
 		# (1) set all columns as output low
@@ -34,6 +37,10 @@ class matrixQPi:
 		# (2) set all rows as input
 		for i in range(len(row)):
 			wiringpi.pinMode(row[i],INPUT)
+		
+		self.keyPad = keyPad
+		self.row = row
+		self.col = col
 
 	def turnOnLED(self):
 		wiringpi.pinMode(led,OUTPUT)
@@ -43,40 +50,41 @@ class matrixQPi:
         
 	def reInit(self):
 		# reinitialize all rows and columns as input before exiting
-		for i in range(len(row)):
-			wiringpi.pinMode(row[i],INPUT)
-		for j in range(len(col)):
-			wiringpi.pinMode(col[j],INPUT)
+		for i in range(len(self.row)):
+			wiringpi.pinMode(self.row[i],INPUT)
+		for j in range(len(self.col)):
+			wiringpi.pinMode(self.col[j],INPUT)
 
 	def scanQ(self):
 		# (3) scan rows for pushed key/button
 		rowHi=1
 		while rowHi==1:
-			for i in range(len(row)):
-				tmpRead=wiringpi.digitalRead(row[i])
+			for i in range(len(self.row)):
+				tmpRead=wiringpi.digitalRead(self.row[i])
 				if tmpRead==0:
 					rowHi=0
 					rowVal=i
 
 		# (4) after finding which key/button from the row scans, convert columns to input
-		for j in range(len(col)):
-				wiringpi.pinMode(col[j],INPUT)
+		for j in range(len(self.col)):
+				wiringpi.pinMode(self.col[j],INPUT)
 
 		# (5) switch the i-th row found from scan to output
-		wiringpi.pinMode(row[rowVal],OUTPUT)
-		wiringpi.digitalWrite(row[rowVal],HIGH)
+		wiringpi.pinMode(self.row[rowVal],OUTPUT)
+		wiringpi.digitalWrite(self.row[rowVal],HIGH)
 
 		# (6) scan columns for still-pushed key/button
 		colLo=0
 		while colLo==0:
-				for j in range(len(col)):
-						tmpRead=wiringpi.digitalRead(col[j])
+				for j in range(len(self.col)):
+						tmpRead=wiringpi.digitalRead(self.col[j])
 						if tmpRead==1:
 							colLo=1
 							colVal=j
 
 		# (7) print the symbol of pressed key, note that it prints newline
 		return self.keyPad[rowVal][colVal]
-
+		
+		reInit()
 		#if myArg=="-i":
         #turnOnLED()
